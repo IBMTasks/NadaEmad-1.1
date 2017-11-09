db04dd127696f251637174f863a51b22afaa801a
NadaEmad <nadaemad@feps.edu.eg>
2017-11-09 16:26:48 +0200

    New

--- public/javascripts/ListContacts.js
+++ public/javascripts/ListContacts.js
@@ -1,57 +1,36 @@
 
-var app = angular.module("myApp", ["dataGrid","pagination"]);
-app.factory('myService',function($http){
-	
-	var email = new URLSearchParams(window.location.search).get("_id");
-    console.log(email);
-    
-	var getData = function() 
-	{
-		return $http.get("/list_contacts", {params:{"_id": email}})
-        .then(function (response) {
-        	return response.data;
-        });
-		
-    };
+  angular.module('myApp', [ 'dataGrid','pagination'])
+  .controller('tableController', ['$scope', 'myAppFactory', '$filter', function ($scope, myAppFactory, $filter) {
 
+	  
+          $scope.gridOptions = {
+              data: [],
+              urlSync: true
+          };
 
-    return { getData: getData };
-	
-	
-});
- 
- 
-  app.controller("tableController", demoController);
-  demoController.$inject = ["$scope","myService"];
+          myAppFactory.getData().then(function (responseData) {
+        	  	console.log(responseData);
+              $scope.gridOptions.data = responseData;
+          });
+        
 
-  function demoController($scope, myService) {
-    var self = this;
-    
-    
-    var mydatapromise = myService.getData();
-    mydatapromise.then(function(result){
-    	
-    	$scope.gridOptions = {
-    			data: result,
-    			sort:
-			{
-    				predicate: 'name',
-    				direction: 'asc'
-			}
-    			
-    	};
-    	
-  	});
-   
-    console.log($scope.users);
-    
-  }
-
-
-
-
-
-
+      }])
+      .factory('myAppFactory', function ($http) {
+    	  	var email = new URLSearchParams(window.location.search).get("_id");
+  	    console.log(email);
+          return {
+              getData: function() 
+		      	{
+		      		return $http.get("/list_contacts", {params:{"_id": email}})
+		              .then(function (response) {
+		              	return response.data;
+		              });
+		      		
+		          }
+          
+          }
+      
+      });
 
 
 
